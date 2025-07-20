885e8db07c21e8f94132dd473c989b4fe878a4e0
vincentve42 <vincentve42@gmail.com>
2025-07-20 18:51:02 +0700

    Tailwind css install

---
+++ resources/views/components/navigation-bar.blade.php
@@ -0,0 +1,13 @@
+<div class="flex justify-between shadow-xs">
+    <div>
+        <p>Tentang Kami</p>
+    </div>
+    <div>
+
+    </div>
+    <div>
+        <p>Upload</p>
+        <p>Daftar</p>
+        <p>Sign In</p>
+    </div>
+</div>
