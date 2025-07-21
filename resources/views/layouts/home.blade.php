2276d4541b3dbd93e449559fca0bcc5d897a61d4
vincentve42 <vincentve42@gmail.com>
2025-07-21 13:28:39 +0700

    Guest Navbar UI

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
 
 
 </head>
-<body>  
+<body x-data='{navbar : false, mobile: true}'>  
     <div class="navbar">
         @include('layouts.navigation.navbar')
-        @yield('navbar')
+        
+    </div>
+    <div class="container">
+         @yield('container')
     </div>
 </body>
 </html>
--- resources/views/layouts/navigation/navbar.blade.php
+++ resources/views/layouts/navigation/navbar.blade.php
@@ -1,5 +1,10 @@
-<nav>
+<nav class=''>
+    <div class="" x-show='navbar-mobile'>
+        
+    </div>
 
+   
+        
     {{-- Desktop View --}}
         <div class=" justify-between shadow-xs hidden lg:flex">
             <div class="items-center justify-items-center pl-6">
@@ -56,7 +61,92 @@
                     
                     <li class="bg-indigo-500 text-white p-1 pl-2 pr-2">Daftar Sekarang</li>
                     </div>
+                    
+                    </div>
                 </ul>
             </div>
+        </div>
+    </nav>
+
+    <nav x-show='mobile' class="shadow-xs">
+         {{-- Mobile Button --}}
+    <div  class="lg:hidden flex justify-between items-center justify-items-center">
+        <div class="p-4"><button class="" @click='mobile = !mobile; navbar=!navbar'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+            </button>
+
+        </div>
+        <div class="p-4">
+            <p>Logo</p>
+        </div>
+        <div>
+            <ul class="flex">
+                <li class="pr-2">
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                        </svg>
+                </li>
+                <li>
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                        </svg>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <nav x-show='navbar' class="shadow-2xl transition duration-750 ease-in-out">
+        <div class="flex justify-between items-center justify-items-center">
+            <div class="p-4">
+                <p>Logo</p>
+            </div>
+            <div class="p-4">
+                <button class="" @click='mobile = !mobile; navbar=!navbar'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-red-500">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg></button>
+
+            </div>
+        </div>
+        <div>
+            <ul>
+                <div class="p-3 flex">
+                     <div class="pr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                        </svg>
+                    </div>
+                    <li >Tentang Kami</li>
+                </div>
+                <div class="p-3 flex">
+                     <div class="pr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                    </svg>
+
+                    </div>
+                    <li >Upload</li>
+                </div>
+               <div class="p-3 flex">
+                     <div class="pr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                        </svg>
+
+                    </div>
+                    <li >Masuk</li>
+                </div>
+                <div class="p-3 flex">
+                     <div class="pr-1">
+                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                        </svg>
+
+
+                    </div>
+                    <li >Create Account</li>
+                </div>
+            </ul>
         </div>
     </nav>
